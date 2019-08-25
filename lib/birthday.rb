717fa3ed456ca7f653ec38576cd7df3b33e3a281
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-25 18:01:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ def happy_birthday(birthday_kids)
 end
 
 
-    }
+
 
     expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
 
