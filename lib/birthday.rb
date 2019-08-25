1fcd8c70702037ec36e00ef1d5efb299873c0799
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-25 18:11:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
   "#age_appropriate_birthday" do
-  xit "only prints the birthday greeting if the birthday kid 12 or younger" 
+  it "only prints the birthday greeting if the birthday kid 12 or younger" 
     else
       puts "You are too old for this."
     end
